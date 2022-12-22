b7c5f7b3abc9ce49b2e1411d702b2467582563fd
Marco Di Lisa <marco.di.lisa@nttdata.com>
2022-12-22 11:17:34 +0100

    Return non null values

--- src/main/java/it/gov/pagopa/iban/dto/IbanDTO.java
+++ src/main/java/it/gov/pagopa/iban/dto/IbanDTO.java
@@ -1,10 +1,13 @@
 package it.gov.pagopa.iban.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@JsonInclude(Include.NON_NULL)
 public class IbanDTO {
   private String iban;
   private String checkIbanStatus;
