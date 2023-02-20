0d90122ebfbdbfd3522107e7de1cb2744724a7b7
pelliccm <matteo.pelliccione@nttdata.com>
2023-02-20 18:27:45 +0100

    userId initiativeId in message header

--- src/main/java/it/gov/pagopa/iban/event/producer/IbanProducer.java
+++ src/main/java/it/gov/pagopa/iban/event/producer/IbanProducer.java
@@ -4,17 +4,25 @@ import it.gov.pagopa.iban.dto.IbanQueueWalletDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IbanProducer {
-  @Value("${spring.cloud.stream.bindings.IbanQueue-out-0.binder}")
-  private String binderIban;
-  @Autowired
-  StreamBridge streamBridge;
+    @Value("${spring.cloud.stream.bindings.IbanQueue-out-0.binder}")
+    private String binderIban;
+    @Autowired
+    StreamBridge streamBridge;
 
-  public void sendIban(IbanQueueWalletDTO iban){
-    streamBridge.send("IbanQueue-out-0", binderIban, iban);
-  }
+    public void sendIban(IbanQueueWalletDTO iban) {
+        streamBridge.send("IbanQueue-out-0", binderIban, buildMessage(iban));
+    }
 
+    public static Message<IbanQueueWalletDTO> buildMessage(IbanQueueWalletDTO ibanQueueWalletDTO) {
+        return MessageBuilder.withPayload(ibanQueueWalletDTO)
+                .setHeader(KafkaHeaders.MESSAGE_KEY, "%s_%s".formatted(ibanQueueWalletDTO.getUserId(), ibanQueueWalletDTO.getInitiativeId()))
+                .build();
+    }
 }
